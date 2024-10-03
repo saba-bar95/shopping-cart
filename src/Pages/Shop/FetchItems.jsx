0c0cd9cf42d42cef1f38a1b510196e21e2b993a8
saba-bar95 <s.barbakadze95@gmail.com>
2024-10-03 17:49:05 +0400

    Display clothes

--- src/Pages/Shop/FetchItems.jsx
+++ src/Pages/Shop/FetchItems.jsx
@@ -1,7 +1,5 @@
 const FetchItems = () => {
-  return fetch("https://api.escuelajs.co/api/v1/categories/1/products").then(
-    (res) => res.json()
-  );
+  return fetch("https://fakestoreapi.com/products").then((res) => res.json());
 };
 
 export default FetchItems;
--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,6 +1,46 @@
 import Navbar from "../../Components/Navbar/Navbar";
 import FetchItems from "./FetchItems";
 import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const StyledContainer = styled.div`
+  width: 80%;
+  margin: auto;
+  display: grid;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+  gap: 30px;
+  padding: 50px 0;
+`;
+
+const StyledItemContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  gap: 10px;
+  padding: 20px;
+`;
+
+const StyledHeader = styled.h1`
+  font-size: 1em;
+  flex: 1;
+`;
+
+const ImgContainer = styled.div`
+  padding: 20px;
+  border: 5px solid #e2e8f0;
+  width: 25vw; /* 25% of the viewport width */
+  height: 25vw; /* 25% of the viewport width */
+  &:hover {
+    /* your hover styles here */
+  }
+`;
+
+const StyledImg = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: contain; /* scale the image to cover the entire container */
+`;
 
 const Shop = () => {
   const [items, setItems] = useState([]);
@@ -19,6 +59,19 @@ const Shop = () => {
   return (
     <>
       <Navbar />
+      <StyledContainer>
+        {items &&
+          items.map((item, index) => {
+            return (
+              <StyledItemContainer className="item-container" key={index}>
+                <ImgContainer>
+                  <StyledImg src={item.image} alt="" />
+                </ImgContainer>
+                <StyledHeader>{item.title}</StyledHeader>
+              </StyledItemContainer>
+            );
+          })}
+      </StyledContainer>
     </>
   );
 };
